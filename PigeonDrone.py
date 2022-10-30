b125c1546dbf6159f5330b92cb7705f2b577a2e6
Krish Iyengar <akrsappsllc@gmail.com>
2022-10-30 12:47:20 -0700

    Updated

--- PigeonDrone.py
+++ PigeonDrone.py
@@ -20,13 +20,17 @@ class PigeonDrone:
     @staticmethod
     def setPigeonAlt(y):
         PigeonDrone.pigeon.simple_goto(LocationGlobalRelative(PigeonDrone.PigeonDrone.pigeon.location.lat, PigeonDrone.PigeonDrone.pigeon.location.long), y)
-        #pigeonVelocityCommand = pigeon.message_factory.set_position_target_local_ned_encode(0, 0, 0, mavutil.mavlink.MAV_FRAME_BODY_NED, 0b0000111111000111, 0, 0, 0, x, y, z, 0, 0, 0, 0, 0)
         print("Created Pigeon Command")
         
        
         #PigeonDrone.pigeon.send_mavlink(pigeonVelocityCommand)
         #PigeonDrone.pigeon.flush()
-            
-
+        
+    def updatePigeonVelocities(x, y, z):
+        if x != 0 or y != 0 or z != 0:
+            pigeonVelocityCommand = PigeonDrone.pigeon.message_factory.set_position_target_local_ned_encode(0, 0, 0, mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED, 0b0000111111000111, 0, 0, 0, x*3, y*3, z*3, 0, 0, 0, 0, 0)
+            PigeonDrone.pigeon.send_mavlink(pigeonVelocityCommand)
+            PigeonDrone.pigeon.flush()
+        
 #pigeon = startPigeonConnect()
 #startMotors()
--- PigeonFirebase.py
+++ PigeonFirebase.py
@@ -2,6 +2,8 @@ import firebase_admin
 from firebase_admin import credentials, db
 import PigeonDrone
 from dronekit import connect, VehicleMode, LocationGlobalRelative, APIException
+import _thread
+import datetime, time
 
 class PigeonFirebase:
     
@@ -16,46 +18,44 @@ class PigeonFirebase:
         PigeonFirebase.pigeonDatabaseControls = db.reference("controls")
         PigeonFirebase.pigeonDatabaseLogs = db.reference("logs")
 
-        def firebasePigeonValuesWrote(pigeonEventListener):
-            print(pigeonEventListener.path)
-            #for one in (pigeon_snapshot):
-             #   print(one.reference.get().to_dict())
+        PigeonDrone.PigeonDrone.pigeon.parameters.add_attribute_listener("*", PigeonFirebase.startPigeonLogs)
+        #PigeonFirebase.pigoneDatabaseControls.update({"altitude": PigeonDrone.PigeonDrone.pigeon.location.global_relative_frame.alt})
+        
+        _thread.start_new_thread(PigeonFirebase.handlePigeonMovements, ())
+        print("WrotePigeon Monitering Database")
+        
+    @staticmethod
+    def handlePigeonMovements():
+        while True:
             pigeonValues = PigeonFirebase.pigeonDatabaseControls.get()
-            print((pigeonEventListener.path))
+
             if (pigeonValues.get("action")) == "Takeoff":
-               # PigeonDrone.PigeonDrone.pigeon.armed = True
-               # PigeonDrone.PigeonDrone.pigeon.mode = VehicleMode("GUIDED")
-               # PigeonDrone.PigeonDrone.pigeon.simple_takeoff(2)
+                PigeonDrone.PigeonDrone.pigeon.armed = True
+                PigeonDrone.PigeonDrone.pigeon.mode = VehicleMode("GUIDED")
+                PigeonDrone.PigeonDrone.pigeon.simple_takeoff(2)
                 PigeonFirebase.pigeonDatabaseControls.update({"action": ""})
 
                 print("HELLO")
             elif (pigeonValues.get("action")) == "Land":
                 print("HELLO")
-               # PigeonDrone.PigeonDrone.pigeon.mode = VehicleMode("LAND")
+                PigeonDrone.PigeonDrone.pigeon.mode = VehicleMode("LAND")
                 PigeonFirebase.pigeonDatabaseControls.update({"action": ""})
 
             elif  (pigeonValues.get("action")) == "Return to Home":
-               # PigeonDrone.PigeonDrone.pigeon.mode = VehicleMode("RTL")
+                PigeonDrone.PigeonDrone.pigeon.mode = VehicleMode("RTL")
                 PigeonFirebase.pigeonDatabaseControls.update({"action": ""})
 
                 print("Boom")
 
             else:
-                if pigeonValues.get("altitude") != 0:
-                    print("Take off to")
-                    print(PigeonDrone.PigeonDrone.pigeon.location.global_relative_frame.alt + pigeonValues.get("altitude"))
-                    #setPigeonAlt(PigeonDrone.PigeonDrone.pigeon.location.global_relative_frame.alt + pigeonValues.get("altitude"))
+                
+                PigeonDrone.PigeonDrone.updatePigeonVelocities(pigeonValues.get("x_vel"), pigeonValues.get("y_vel"), pigeonValues.get("z_vel"))
+                    
                 print("Read Values")
-                #setPigeonVelocity(pigeonValues.get("x_vel"), pigeonValues.get("y_vel"), pigeonValues.get("z_vel"))
-        PigeonFirebase.pigeonDatabaseControls.listen(firebasePigeonValuesWrote)
-        
-        PigeonDrone.PigeonDrone.pigeon.parameters.add_attribute_listener("*", PigeonFirebase.startPigeonLogs)
-        #PigeonFirebase.pigoneDatabaseControls.update({"altitude": PigeonDrone.PigeonDrone.pigeon.location.global_relative_frame.alt})
-        print("WrotePigeon Monitering Database")
-        
-    
+            time.sleep(0.5)
+
     def startPigeonLogs(self, pigeonParamName, pigeonParamValue):
         
         pigeonLocData = PigeonDrone.PigeonDrone.pigeon.location.global_relative_frame
         print(PigeonDrone.PigeonDrone.pigeon.attitude)
-        PigeonFirebase.pigeonDatabaseLogs.update({"mode": PigeonDrone.PigeonDrone.pigeon.mode.name, "pitch": PigeonDrone.PigeonDrone.pigeon.attitude.pitch, "yaw": PigeonDrone.PigeonDrone.pigeon.attitude.yaw, "roll": PigeonDrone.PigeonDrone.pigeon.attitude.roll, "airspeed": PigeonDrone.PigeonDrone.pigeon.airspeed, "groundspeed": PigeonDrone.PigeonDrone.pigeon.groundspeed, "lastLink": PigeonDrone.PigeonDrone.pigeon.last_heartbeat, "altitude": pigeonLocData.alt, "lat": pigeonLocData.lat, "long": pigeonLocData.lon, "battery": PigeonDrone.PigeonDrone.pigeon.battery.level, "velocity": PigeonDrone.PigeonDrone.pigeon.velocity})
+        PigeonFirebase.pigeonDatabaseLogs.update({"lastUpdateDate": str(datetime.datetime.now()), "mode": PigeonDrone.PigeonDrone.pigeon.mode.name, "pitch": PigeonDrone.PigeonDrone.pigeon.attitude.pitch, "yaw": PigeonDrone.PigeonDrone.pigeon.attitude.yaw, "roll": PigeonDrone.PigeonDrone.pigeon.attitude.roll, "airspeed": PigeonDrone.PigeonDrone.pigeon.airspeed, "groundspeed": PigeonDrone.PigeonDrone.pigeon.groundspeed, "lastLink": PigeonDrone.PigeonDrone.pigeon.last_heartbeat, "altitude": pigeonLocData.alt, "lat": pigeonLocData.lat, "long": pigeonLocData.lon, "battery": PigeonDrone.PigeonDrone.pigeon.battery.level, "velocity": PigeonDrone.PigeonDrone.pigeon.velocity})
