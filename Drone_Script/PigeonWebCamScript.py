c3fae8244b648d47966769df5c409a96e5055108
Krish Iyengar <akrsappsllc@gmail.com>
2022-11-02 17:51:03 -0700

    Hello

--- Drone_Script/PigeonWebCamScript.py
+++ Drone_Script/PigeonWebCamScript.py
@@ -2,24 +2,25 @@ from flask import Flask, render_template, Response
 import numpy
 import cv2
 
-class PigeonWebCam:
     
-    pigeonWebApp = Flask(__name__)
-    pigeonWebVideoCam = cv2.VideoCapture(0)
+pigeonWebApp = Flask(__name__)
+pigeonWebVideoCam = cv2.VideoCapture(0)
+
+@pigeonWebApp.route("/")
+def pigeonWebCamFlask():
+    return render_template("PigeonWebCam.html")
+
+def pigeonWebCamGen():
+    while True:
+        pigeonSuccessful, pigeonWebFrame = pigeonWebVideoCam.read()
+        cv2.imwrite("pigeonFrame.jpg", pigeonWebFrame)
+        yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + open("pigeonFrame.jpg", "rb").read() + b'\r\n')
+
+@pigeonWebApp.route("/pigeonVideoFeed")
+def pigeonVideoFeed():
+    return Response(pigeonWebCamGen(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+def startPigeonWebCam():
+    pigeonWebApp.run(host="10.0.0.147", use_reloader=False)
     
-    @app.route("/")
-    def startPigeonWebCamFlask():
-        return render_template("PigeonWebCam.html")
-    
-    def pigeonWebCamGen():
-        while True:
-            pigeonSuccessful, pigeonWebFrame = pigeonWebVideoCam.read()
-            cv2.imwrite("pigeonFrame.png", pigeonWebFrame)
-            yield(b'--frame\r\n'b'Content-Type: imaghe/jpeg\r\n\r\n' + open("pigeonFrame.png", "rb").read() + b'\r\n')
-    
-    @app.route("/pigeonVideoFeed")
-    def pigeonVideoFeed():
-        return Response(pigeonWebCamGen(), mimetype="multipart/x-mixed-replace; boundary=frame")
-    
-    if __name__ == "__main__":
-        pigeonWebApp.run(host="0.0.0.0", debug=True)
+startPigeonWebCam()
