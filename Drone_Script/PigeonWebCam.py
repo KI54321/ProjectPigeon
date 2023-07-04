3f8b9d507a0539670ef2a616858dc404e19d0b53
Krish Iyengar <103964428+KI54321@users.noreply.github.com>
2023-07-04 13:57:05 -0700

    Update PigeonWebCam.py

--- Drone_Script/PigeonWebCam.py
+++ Drone_Script/PigeonWebCam.py
@@ -22,5 +22,5 @@ def pigeonVideoFeed():
     return Response(pigeonWebCamGen(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 if __name__ == "__main__":
-    app.run(host="10.0.0.147")
+    app.run(host="XXXX")
 
